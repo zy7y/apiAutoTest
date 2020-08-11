3c70277e18876403a8d017682edc5d563c958581
zy7y <7631909+zy7y@user.noreply.gitee.com>
2020-08-12 01:56:58 +0800

    增加注释说明

--- test/test_api.py
+++ test/test_api.py
@@ -20,14 +20,12 @@ from tools.save_response import SaveResponse
 
 # 读取配置文件 对象
 rc = ReadConfig()
-log_path = rc.read_file_path('log_path')
-logger.add(log_path)
 base_url = rc.read_serve_config('dev')
 token_reg, res_reg = rc.read_response_reg()
 case_data_path = rc.read_file_path('case_data')
 report_data = rc.read_file_path('report_data')
 report_generate = rc.read_file_path('report_generate')
-
+log_path = rc.read_file_path('log_path')
 report_zip = rc.read_file_path('report_zip')
 email_setting = rc.read_email_setting()
 # 实例化存响应的对象
@@ -38,7 +36,7 @@ data_list = ReadData(case_data_path).get_data()
 treat_data = TreatingData()
 # 请求对象
 br = BaseRequest()
-logger.info(f'文件读取完毕\n\n')
+logger.info(f'配置文件/excel数据/对象实例化，等前置条件处理完毕\n\n')
 
 
 class TestApiAuto(object):
@@ -59,6 +57,27 @@ class TestApiAuto(object):
                              'file_path, parameters, dependent,data,expect', data_list)
     def test_main(self, case_number, case_title,path, is_token, method, parametric_key, file_var, file_path, parameters,
                   dependent, data, expect):
+        """
+        :param case_number: 用例编号
+        :param case_title: 用例标题
+        :param path: 接口路径
+        :param is_token: token操作：写入token/读取token/不携带token
+        :param method: 请求方式：get/post/put/delete....
+        :param parametric_key: 入参关键字：params/data/json
+        :param file_var: 接口中接受文件对象的参数名称
+        :param file_path: 文件路径，单文件实例：/Users/zy7y/PycharmProjects/apiAutoTest/test/__init__.py
+        多文件实例['/Users/zy7y/PycharmProjects/apiAutoTest/test/__init__.py','/Users/zy7y/PycharmProjects/apiAutoTest/test/test_api.py']
+
+        :param parameters: path参数(携带在url中的参数)依赖处理 users/:id(id携带在url中) 实例:{"case_001": '$.data.id'}，解析
+        从用例编号为case_001的实际结果响应中提取data字典里面的id的内容(假设提取出来是500), 最后请求的路径将是host + users/500
+
+        :param dependent: data数据依赖，该接口需要上一个接口返回的响应中的某个字段及内容：实例{"case_001",["$.data.id","$.data.username"]}
+        解析: 从用例case_001的实际响应结果中提取到data下面的id，与username的值(假设id值为500，username为admin)，那么提取的数据依赖内容将是{"id":500, "username":"admin"}
+        纳闷最终请求的data 将是 {"id":500, "username":"admin"} 与本身的data合并后的内容
+        :param data: 请求数据
+        :param expect:预期结果，最后与config/config.yaml下的response_reg->response提取出来的实际响应内容做对比，实现断言
+        :return:
+        """
 
         # 感谢：https://www.cnblogs.com/yoyoketang/p/13386145.html，提供动态添加标题的实例代码
         # 动态添加标题
@@ -89,7 +108,7 @@ class TestApiAuto(object):
 if __name__ == '__main__':
     TestApiAuto().run_test()
 
-    # 使用jenkins集成将不会使用到这两个方法
+    # 使用jenkins集成将不会使用到这两个方法（邮件发送/报告压缩zip）
     # from tools.zip_file import zipDir
     # from tools.send_email import send_email
     # zipDir(report_generate, report_zip)
