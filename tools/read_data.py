5626ac52aa55ab25d71fd64b8288f7d3e352824c
zy7y <7631909+zy7y@user.noreply.gitee.com>
2020-08-20 10:36:56 +0800

    优化if逻辑语句，移除continue

--- tools/read_data.py
+++ tools/read_data.py
@@ -25,12 +25,11 @@ class ReadData(object):
         table = self.book.sheet_by_index(0)
         for norw in range(1, table.nrows):
             # 每行第4列 是否运行
-            if table.cell_value(norw, 3) == '否':
-                continue
-            value = table.row_values(norw)
-            value.pop(3)
-            # 配合将每一行转换成元组存储，迎合 pytest的参数化操作，如不需要可以注释掉 value = tuple(value)
-            value = tuple(value)
-            logger.info(f'{value}')
-            data_list.append(value)
+            if table.cell_value(norw, 3) != '否':  # 每行第三列等于否将不读取内容
+                value = table.row_values(norw)
+                value.pop(3)
+                # 配合将每一行转换成元组存储，迎合 pytest的参数化操作，如不需要可以注释掉 value = tuple(value)
+                value = tuple(value)
+                logger.info(f'{value}')
+                data_list.append(value)
         return data_list
