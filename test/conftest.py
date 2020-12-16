6120aa3ee8f1c4831339753a8d2adb07b5bbcff1
zy7y <7631909+zy7y@user.noreply.gitee.com>
2020-12-16 10:05:05 +0800

    添加初始化用例操作

--- test/conftest.py
+++ test/conftest.py
@@ -12,11 +12,22 @@
 import pytest
 
 from tools.db import DB
+from tools.read_file import ReadFile
 
 
 @pytest.fixture(scope="session")
 def get_db():
     """关于其作用域请移步查看官方文档"""
-    db = DB()
-    yield db
-    db.close()
+    try:
+        db = DB()
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(params=ReadFile.read_testcase())
+def cases(request):
+    """用例数据，测试方法参数入参该方法名 cases即可，实现同样的参数化
+    目前来看相较于@pytest.mark.parametrize 更简洁。
+    """
+    return request.param
